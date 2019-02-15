7217158cbdcf3b460ddbd2682dd948b75d6eda8a
Gianni <gianni.codevico@gmail.com>
2019-02-15 14:22:53 +0100

    Added db service

--- src/main/java/sample/DbService.java
+++ src/main/java/sample/DbService.java
@@ -1,7 +1,32 @@
 package sample;
 
-import Moka7.S7Client;
+import java.sql.*;
 
 public class DbService {
+    private static DbService ourInstance = new DbService();
+    public static DbService getInstance() {
+        return ourInstance;
+    }
 
+    // Create a variable for the connection string.
+    String connectionUrl = "jdbc:sqlserver://<server>:<port>;databaseName=AdventureWorks;user=<user>;password=<password>";
+
+    Connection con;
+
+    private DbService()
+    {
+        try {
+            con = DriverManager.getConnection(connectionUrl);
+            Statement stmt = con.createStatement();
+            String SQL = "SELECT TOP 10 * FROM Person.Contact";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            // Iterate through the data in the result set and display it.
+            while (rs.next()) {
+                System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/sample/RigaWO.java
@@ -0,0 +1,57 @@
+package sample;
+
+import com.univocity.parsers.annotations.NullString;
+import com.univocity.parsers.annotations.Parsed;
+
+public class RigaWO {
+
+    // if the value parsed in the quantity column is "?" or "-", it will be replaced
+    // by null.
+    @NullString(nulls = { "?", "-" })
+    // if a value resolves to null, it will be converted to the String "0".
+
+    @Parsed(field = "wo")
+    private String wo;
+
+    @Parsed(field = "TipoArt")
+    private String TipoArt;
+
+    @Parsed(field = "Articolo")
+    private String Articolo;
+
+    @Parsed(field = "Descrizione")
+    private String Descrizione;
+
+
+    public String getWo() {
+        return wo;
+    }
+
+    public void setWo(String wo) {
+        this.wo = wo;
+    }
+
+    public String getTipoArt() {
+        return TipoArt;
+    }
+
+    public void setTipoArt(String tipoArt) {
+        TipoArt = tipoArt;
+    }
+
+    public String getArticolo() {
+        return Articolo;
+    }
+
+    public void setArticolo(String articolo) {
+        Articolo = articolo;
+    }
+
+    public String getDescrizione() {
+        return Descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        Descrizione = descrizione;
+    }
+}
